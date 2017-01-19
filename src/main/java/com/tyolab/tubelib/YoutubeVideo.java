0199cd94cef5fc175fd3027d3b6edbd8fa16bde3
tyodev <deve@tyo.com.au>
2017-01-19 11:31:34 +1000

    minor updates on YoutubeVideo class

--- src/main/java/com/tyolab/tubelib/YoutubeVideo.java
+++ src/main/java/com/tyolab/tubelib/YoutubeVideo.java
@@ -1,12 +1,30 @@
 package com.tyolab.tubelib;
 
-import java.util.ArrayList;
-
 /**
  * Created by "Eric Tang (dev@tyo.com.au)" on 16/1/17.
  */
 
 public class YoutubeVideo extends VideoItem {
+
+    private static String protocol = "https";
+    private static String host = "img.youtube.com";
+    private static String path = "/vi/";
+
+    private static String imgExtension = "jpg";
+
+    public static void setYoutubeHost(String host, String path, String protocol) {
+        YoutubeVideo.path = path;
+        YoutubeVideo.host = host;
+        YoutubeVideo.protocol = protocol;
+
+        buildUrl();
+    }
+
+    private static String baseUrl = buildUrl();
+
+    private static String buildUrl() {
+       return baseUrl = protocol + "://" + host + path;
+    }
 
     /**
      *
@@ -26,6 +44,8 @@ public class YoutubeVideo extends VideoItem {
 
     private int INDEX_HIGH_QUALITY = 5;
 
+    private int INDEX_STANDARD_QUALITY = 7;
+
     /**
      *
      */
@@ -38,7 +58,7 @@ public class YoutubeVideo extends VideoItem {
      *
      * md
      */
-    private ArrayList<String> thumbnailUrls;
+//    private List<String> thumbnailUrls;
 
     public YoutubeVideo(String id) {
         super(id);
@@ -49,7 +69,54 @@ public class YoutubeVideo extends VideoItem {
         this.setUrl(url);
     }
 
-    public void createThumbnailImages() {
+//    public void createThumbnailImages() {
+//        thumbnailUrls = new ArrayList<String>(8);
+//
+//        for (int i = 0; i < 4; ++i)
+//            thumbnailUrls.set(i, "" + i);
+//    }
 
+    private String getThumbnailWithName(String name) {
+        return baseUrl + getId() + "/" + name + "." + imgExtension;
+    }
+
+    public String getThumbnail(int index) {
+        return getThumbnailWithName(String.valueOf(index));
+    }
+
+    public String getThumbnailOne() {
+        return getThumbnail(1);
+    }
+
+    public String getThumbnailTwo() {
+        return getThumbnail(2);
+    }
+
+    public String getThumbnailThree() {
+        return getThumbnail(3);
+    }
+
+    public String getThumbnailFour() {
+        return getThumbnail(4);
+    }
+
+    public String getThumbnailDefault() {
+        return getThumbnailWithName("default");
+    }
+
+    public String getThumbnailHighQuality() {
+        return getThumbnailWithName("hqdefault");
+    }
+
+    public String getThumbnailStandard() {
+        return getThumbnailWithName("sddefault");
+    }
+
+    public String getThumbnailMediumQuality() {
+        return getThumbnailWithName("mqdefault");
+    }
+
+    public String getThumbnailMaximunResolution() {
+        return getThumbnailWithName("maxresdefault");
     }
 }
